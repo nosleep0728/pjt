95492193a6d13006d42ed6d3cd3c9b6b4d24b1f1
nosleep0728 <nosleep0728@gmail.com>
2015-01-04 22:59:21 +0900

    2015.01.04

--- csharp/CodingHelper/CodingHelper/FrmSetting.Designer.cs
+++ csharp/CodingHelper/CodingHelper/FrmSetting.Designer.cs
@@ -28,20 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmSetting));
+            this.btnClose = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.miClose = new System.Windows.Forms.ToolStripMenuItem();
+            this.miSet = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(49, 230);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "닫기";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miClose,
+            this.miSet});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(99, 48);
+            // 
+            // miClose
+            // 
+            this.miClose.Name = "miClose";
+            this.miClose.Size = new System.Drawing.Size(152, 22);
+            this.miClose.Text = "닫기";
+            this.miClose.Click += new System.EventHandler(this.menuItem_Click);
+            // 
+            // miSet
+            // 
+            this.miSet.Name = "miSet";
+            this.miSet.Size = new System.Drawing.Size(152, 22);
+            this.miSet.Text = "설정";
+            this.miSet.Click += new System.EventHandler(this.menuItem_Click);
             // 
             // FrmSetting
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(426, 273);
+            this.ClientSize = new System.Drawing.Size(446, 265);
+            this.Controls.Add(this.btnClose);
             this.Name = "FrmSetting";
             this.Text = "설정";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FrmSetting_FormClosed);
+            this.Load += new System.EventHandler(this.FrmSetting_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem miClose;
+        private System.Windows.Forms.ToolStripMenuItem miSet;
     }
 }
 
